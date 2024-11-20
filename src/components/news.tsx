89f4d669fa9ada1b68446723004731c595ce6145
Andrew Marshall <andym234@gmail.com>
2024-11-20 16:57:56 +0000

    updated news items

--- src/components/news.tsx
+++ src/components/news.tsx
@@ -9,25 +9,22 @@ function News() {
         <div id="news">
             <h1>latest news</h1>
             <Row xs={1} sm={1} md={3} lg={3} className="g-4 align-items-start">
-                {/* TODO change */}
                 <NewsItem
-                    title="VOLUNTEERING"
-                    openingLine="Volunteer days are planned to help keep the club clean and tidy over winter."
-                    mainText="These will be every other Saturday between 10am and 12pm, starting on the 19th October. Members and non-members are welcome to help out."
+                    title="BOWLS MEETING"
+                    openingLine="There will be a bowls meeting on the 23rd November at 2pm."
+                    mainText="The meeting will be held at the club and all members are encouraged to attend. The meeting will discuss the the teams for the upcoming season and to choose the captains."
                     imgSrc={volunteeringImage}
                 />
                 <NewsItem
-                    title="LADIES TEAM"
-                    openingLine="We are looking to enter a team into the Leeds Ladies League for the 2025 season."
-                    mainText="If you are interested in playing then please contact a club member. No experience is necessary."
+                    title="SPONSORSHIP PACKAGES"
+                    openingLine="Sponsorship packages are available for the 2025 season."
+                    mainText="If you are interested then please contact a member of the club for more information."
                     imgSrc={ladiesTeamImage}
                 />
                 <NewsItem
-                    title="AGM"
-                    openingLine="The 2024 AGM was held on the 9th November."
-                    mainText="The club has decided to enter the AireWharfe league on a Monday evening instead of the Leeds Combined league.
-                    The team captains remain the same as last year, with Nicola Bona being chosen to captain the new ladies team on a Thursday, 
-                    and Stuart Potter was voted onto the committee."
+                    title="UPCOMING EVENTS"
+                    openingLine="The Christmas party and Presentation night have been scheduled."
+                    mainText="The Christmas party has been booked for the 13th December, and Presentation night will be on the 1st February (both starting at 6pm)"
                     imgSrc={agmImage}
                 />
             </Row>
