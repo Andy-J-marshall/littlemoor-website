0d9cfd102eee438b59caf78c70d72d4b4f79ada4
Andrew Marshall <andym234@gmail.com>
2024-11-21 00:54:48 +0000

    refactored how news items are displayed and stored

--- src/components/news.tsx
+++ src/components/news.tsx
@@ -1,32 +1,113 @@
-import { Row } from 'react-bootstrap';
-import NewsItem from './newsItem';
-import news1Img from '../images/news/news1.png';
-import news2Img from '../images/news/news2.png';
-import news3Img from '../images/news/news3.png';
+import { useState } from 'react';
+import { Button, Card, Collapse, Col, Row } from 'react-bootstrap';
+import news1Img from '../clubNews/news-image1.png';
+import news2Img from '../clubNews/news-image2.png';
+import news3Img from '../clubNews/news-image3.png';
+import news from '../clubNews/news.json';
 
 function News() {
+    const [expandNews1, setExpandNews1] = useState(false);
+    const [expandNews2, setExpandNews2] = useState(false);
+    const [expandNews3, setExpandNews3] = useState(false);
+
+    function handleNewsExpand1() {
+        setExpandNews1(!expandNews1);
+        setExpandNews2(false);
+        setExpandNews3(false);
+    }
+
+    function handleNewsExpand2() {
+        setExpandNews2(!expandNews2);
+        setExpandNews1(false);
+        setExpandNews3(false);
+    }
+
+    function handleNewsExpand3() {
+        setExpandNews3(!expandNews3);
+        setExpandNews1(false);
+        setExpandNews2(false);
+    }
+
+    const newsItems = [
+        {
+            title: news[0].title,
+            openingLine: news[0].openingLine,
+            mainText: news[0].mainText,
+            imgSrc: news1Img,
+            callback: handleNewsExpand1,
+            open: expandNews1,
+        },
+        {
+            title: news[1].title,
+            openingLine: news[1].openingLine,
+            mainText: news[1].mainText,
+            imgSrc: news2Img,
+            callback: handleNewsExpand2,
+            open: expandNews2,
+        },
+        {
+            title: news[2].title,
+            openingLine: news[2].openingLine,
+            mainText: news[2].mainText,
+            imgSrc: news3Img,
+            callback: handleNewsExpand3,
+            open: expandNews3,
+        },
+    ];
+
     return (
         <div id="news">
             <h1>latest news</h1>
             <Row xs={1} sm={1} md={3} lg={3} className="g-4 align-items-start">
-                <NewsItem
-                    title="BOWLS MEETING"
-                    openingLine="There is a bowls meeting on the 23rd November at 2pm."
-                    mainText="The meeting will be held at the club and all members are encouraged to attend. The meeting will discuss the the teams for the upcoming season and to choose the captains."
-                    imgSrc={news3Img}
-                />
-                <NewsItem
-                    title="SPONSORSHIP PACKAGES"
-                    openingLine="Sponsorship packages are available for the 2025 season."
-                    mainText="If you are interested then please contact a member of the club for more information."
-                    imgSrc={news2Img}
-                />
-                <NewsItem
-                    title="UPCOMING EVENTS"
-                    openingLine="The Presentation night and Christmas party have been booked."
-                    mainText="The Christmas party has been booked for the 13th December, and Presentation night will be on the 1st February (both starting at 6pm)"
-                    imgSrc={news1Img}
-                />
+                {newsItems.map((item, index) => {
+                    return (
+                        <Col key={index}>
+                            <Card style={{ minHeight: '460px' }} bg="light">
+                                <Card.Body>
+                                    <Card.Title>{item.title}</Card.Title>
+                                    <Card.Img
+                                        style={{ paddingBottom: '1rem' }}
+                                        variant="top"
+                                        src={item.imgSrc}
+                                    />
+                                    {item.open && (
+                                        <Card.Text>
+                                            {item.openingLine}
+                                        </Card.Text>
+                                    )}
+                                    {!item.open && (
+                                        <Card.Text>
+                                            {item.openingLine.length > 100 &&
+                                            !item.open
+                                                ? `${item.openingLine.substring(
+                                                      0,
+                                                      100
+                                                  )}...`
+                                                : item.openingLine}
+                                            <Button
+                                                variant="light"
+                                                onClick={() => item.callback()}
+                                            >
+                                                READ MORE
+                                            </Button>
+                                        </Card.Text>
+                                    )}
+                                    <Collapse in={item.open}>
+                                        <Card.Text>{item.mainText}</Card.Text>
+                                    </Collapse>
+                                    {item.open && (
+                                        <Button
+                                            variant="light"
+                                            onClick={() => item.callback()}
+                                        >
+                                            CLOSE
+                                        </Button>
+                                    )}
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    );
+                })}
             </Row>
         </div>
     );
