33a59835604daa7e3352e5d5b12d05b83c2eef1e
Andrew Marshall <andym234@gmail.com>
2024-11-28 19:03:54 +0000

    updated todo comment

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const config = {
     membershipPrice: '35',
     socialLinks: {
         facebookUrl: 'https://www.facebook.com/groups/254439544994129/', // TODO want a better link? and IG too?
-        emailUrl: 'mailto:littlemoorsportsandsocial@gmail.com', // TODO also change the pipeline email
+        emailUrl: 'mailto:littlemoorsportsandsocial@gmail.com', // TODO change this to a real email
         googleMapsUrl: 'https://maps.app.goo.gl/p3aGzHLGT2CCj5rv8',
         googleMapsIframeUrl:
             'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1721.6986669612086!2d-1.6568973!3d53.7898817!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487be14cadb3aec9%3A0xe3451f6a44388437!2sLittlemoor%20Sports%20%26%20Social%20Club!5e1!3m2!1sen!2suk!4v1732118023565!5m2!1sen!2suk',
