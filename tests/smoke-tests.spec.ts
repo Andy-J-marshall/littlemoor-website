41bd79c071ec54e29588ed7e36fb6ba051ee5a7d
Andrew Marshall <andym234@gmail.com>
2024-12-20 18:15:00 +0000

    updated tests

--- tests/smoke-tests.spec.ts
+++ tests/smoke-tests.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 const clubName = 'Littlemoor Sports and Social Club';
 
 test.describe('Each page loads', () => {
-    test('Home', async ({ page }) => {
+    test('Default', async ({ page }) => {
         await page.goto('');
 
         await expect(page).toHaveTitle(`${clubName}`);
@@ -13,6 +13,14 @@ test.describe('Each page loads', () => {
         await expect(page.locator('#footer')).toBeVisible();
         await expect(page.locator('#carousel')).toBeVisible();
         await expect(page.locator('#news')).toBeVisible();
+    });
+
+    test('Home', async ({ page }) => {
+        await page.goto('#/home');
+
+        await expect(page.locator('#home-page')).toBeVisible();
+        await expect(page.locator('#navbar')).toBeVisible();
+        await expect(page.locator('#footer')).toBeVisible();
     });
 
     test('membership', async ({ page }) => {
@@ -35,6 +43,22 @@ test.describe('Each page loads', () => {
         await page.goto('#/contact');
 
         await expect(page.locator('#contact-page')).toBeVisible();
+        await expect(page.locator('#navbar')).toBeVisible();
+        await expect(page.locator('#footer')).toBeVisible();
+    });
+
+    test('Supporters', async ({ page }) => {
+        await page.goto('#/supporters');
+
+        await expect(page.locator('#supporters-page')).toBeVisible();
+        await expect(page.locator('#navbar')).toBeVisible();
+        await expect(page.locator('#footer')).toBeVisible();
+    });
+
+    test('About Us', async ({ page }) => {
+        await page.goto('#/about');
+
+        await expect(page.locator('#about-page')).toBeVisible();
         await expect(page.locator('#navbar')).toBeVisible();
         await expect(page.locator('#footer')).toBeVisible();
     });
