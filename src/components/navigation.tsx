db6e72ba40beb0293fb7e189452c7adbe37681d5
Andy Marshall <32441763+Andy-J-marshall@users.noreply.github.com>
2024-12-25 13:13:23 +0000

    Update navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -12,7 +12,7 @@ function Navigation() {
             variant="light"
             expand="md"
         >
-            <Container className='center-align' fluid>
+            <Container className="center-align" fluid>
                 <Navbar.Brand as={Link} to="/" href="/home">
                     <Image
                         style={{ height: '2.3rem' }}
